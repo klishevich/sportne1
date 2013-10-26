fec1dc25a866f1106535f79c10ba5a8a9abf0695
klisha <mans1@ngs.ru>
2013-10-26 23:01:08 +0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sportne1::Application.config.secret_token = 'd365635e591c1c99709906a0909f654f9a1778ac7a866226c125e22bbb1460b3bfee3404e2c131d6f8fa9b1c20af2a8cfd50289d93a2a385635c2d4ce45ce66e'
