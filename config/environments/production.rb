372b2d4954a77db4e4f95a3d0dafe5f58ccf4277
klisha <mans1@ngs.ru>
2013-12-29 20:57:01 +0400

    new sendmail config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,11 +72,12 @@ Sportne1::Application.configure do
   }
 
 
-  config.action_mailer.default_url_options = { :host => 'example.com' }
+  config.action_mailer.default_url_options = { :host => 'sportne.ru' }
   # ActionMailer Config
   # Setup for production - deliveries, no errors raised
-  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :sendmail
   config.action_mailer.perform_deliveries = true
+  config.action_mailer.sendmail_settings = { :arguments => "-i" }  
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default :charset => "utf-8"
 
